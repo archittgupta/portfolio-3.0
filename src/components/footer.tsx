e8bd93954b168f192b880c8516abc5d1647055e6
Archit Gupta <architgupta539@gmail.com>
2025-03-16 00:23:32 +0530

    added links to the footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,31 +1,26 @@
 "use client";
-import { Github, Twitter, Linkedin } from "lucide-react";
+import { IconBrandGithub, IconBrandTwitter, IconBrandLinkedin } from "@tabler/icons-react";
 
 export default function Footer() {
+  const openLink = (url: string) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
+
   return (
     <footer className="w-full py-6 flex items-center justify-center text-white">
       <div className="flex space-x-6">
-        <a
-          href="https://github.com/archittgupta"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Github className="w-6 h-6 hover:text-gray-400 transition-colors duration-300" />
-        </a>
-        <a
-          href="https://twitter.com/archittgupta"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Twitter className="w-6 h-6 hover:text-gray-400 transition-colors duration-300" />
-        </a>
-        <a
-          href="https://linkedin.com/in/archit-gupta-973b93261"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Linkedin className="w-6 h-6 hover:text-gray-400 transition-colors duration-300" />
-        </a>
+        <IconBrandGithub
+          className="w-6 z-100 h-6 hover:text-gray-400 cursor-pointer transition-colors duration-300"
+          onClick={() => openLink("https://github.com/archittgupta")}
+        />
+        <IconBrandTwitter
+          className="w-6 h-6 z-100 hover:text-gray-400 cursor-pointer transition-colors duration-300"
+          onClick={() => openLink("https://twitter.com/archittgupta")}
+        />
+        <IconBrandLinkedin
+          className="w-6 h-6 z-100 hover:text-gray-400 cursor-pointer transition-colors duration-300"
+          onClick={() => openLink("https://linkedin.com/in/archit-gupta-973b93261")}
+        />
       </div>
     </footer>
   );
